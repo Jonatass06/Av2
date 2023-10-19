93b86fc814d03d54060c31d78a8d454f75f401b5
Jonatas Jackson Goncalves <jonatasg@weg.net>
2023-10-19 13:53:02 -0300

    Rotas Seguras

--- components/ModalLogin.jsx
+++ components/ModalLogin.jsx
@@ -1,5 +1,13 @@
+
+import { useRouter } from "next/dist/client/router";
 export default () => {
 
+    const router = useRouter();
+    function logar(){
+        let usuario = "192837"
+        document.cookie = "logado=" + usuario + ";max-age=max-age-in-seconds:"+60*60*24*3;
+        router.push('/usuario/'+usuario);
+    }
 
     return (
         <div className="w-[700px] h-[650px] bg-white shadow-10b ronded-[5px] flex flex-col justify-center items-center  gap-12">
@@ -8,8 +16,7 @@ export default () => {
                 <input type="text" placeholder="Nome" />
                 <input type="text" placeholder="Senha" />
             </div>
-                <button className="bg-verde w-[370px] h-[80px] text-branco text-[24px] font-alata rounded-[5px]">Continuar</button>
-
+                <button className="bg-verde w-[370px] h-[80px] text-branco text-[24px] font-alata rounded-[5px]" onClick={() => logar()}>Continuar</button>
         </div>
     )
 }
--- pages/usuario/[id].jsx
+++ pages/usuario/[id].jsx
@@ -1,7 +1,13 @@
+import RotaPrivada from "@/components/RotaPrivada"
+import { useRouter } from "next/dist/client/router";
+
 export default() =>{
+    const id = useRouter().query.id
+
     return(
-        <>
-        </>
+        <RotaPrivada id={id}>
+            <div className="bg-black w-16 h-16"></div>
+        </RotaPrivada>
     )
 
 }
